dcbef2821259097c479509cbb498d66db5c7885e
Kane Rogers <kane.m.rogers@gmail.com>
2022-08-01 22:18:30 +1000

    INIT

---
+++ README.md
@@ -0,0 +1,9 @@
+# robo-dialogue
+## What?
+A tiny tool that takes a CSV file of dialogue, "records" it with text-to-speech and saves the result.
+
+## Why?
+Recording dialogue in games is hard and expensive. To iterate on dialogue quickly while integrating the results into the game, we wrote this little tool to let us get the dialogue just right before we pay people money to speak into a microphone.
+
+## How?
+Amazon Polly does all the work for us.
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,33 @@
-fn main() {
-    println!("Hello, world!");
+use serde::Deserialize;
+use std::path::Path;
+
+fn main() {}
+
+#[derive(Deserialize, Debug)]
+struct DialogueLine {
+    ID: usize,
+    Actor: String,
+    Dialogue: String,
+    Trigger: String,
+    Notes: String,
+}
+
+pub fn record<P: AsRef<Path>>(path: P) {
+    let file = std::fs::File::open(path.as_ref()).unwrap();
+    let mut reader = csv::Reader::from_reader(&file);
+    for row in reader.deserialize::<DialogueLine>() {
+        let line = row.unwrap();
+        println!("{} - {}", line.Actor, line.Dialogue);
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    pub fn test_dialogue() {
+        // Open the file, and write it.
+        record("./test_dialogue.csv");
+    }
 }
